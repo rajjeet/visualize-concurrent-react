aee5501cc3f35a1929f008daf4051ee4914e1d42
Rajjeet Phull <rajjeet.phull@gmail.com>
2019-11-10 16:01:02 -0500

    checkpoint

--- src/pages/transitions/index.js
+++ src/pages/transitions/index.js
@@ -35,12 +35,21 @@ const fetchData = () => ({
 export const Transitions = () => {
   let [page, setPage] = useState(0);
   let [resource, setResource] = useState(null);
-  let [startTransition, isPending] = useTransition({ timeoutMs: 3000 });
+  let [resource2, setResource2] = useState(null);
+  let [startTransition, isPending] = useTransition({ timeoutMs: 5000 });
 
-  const goNextPage = useCallback((currPage) => {
-    setPage(currPage + 1);
+  const goNextPageReceding = useCallback((currPage) => {
+    let nextPage = currPage + 1;
+    setPage(nextPage);
+    let resource2 = nextPage % 2 === 1 ? fetchData() : null;
+    setResource2(resource2);
+  }, []);
+
+  const goNextPagePending = useCallback((currPage) => {
+    let nextPage = currPage + 1;
+    setPage(nextPage);
     startTransition(() => {
-      let resource = page % 2 === 1 ? fetchData() : null;
+      let resource = nextPage % 2 === 1 ? fetchData() : null;
       setResource(resource);
     });
   }, []);
@@ -48,24 +57,40 @@ export const Transitions = () => {
   return (
     <>
       <h1>Transitions</h1>
-      <StyledButton onClick={() => goNextPage(page)}>Next</StyledButton>
+      <StyledButton onClick={() => goNextPageReceding(page)}>Next Receding</StyledButton>
+      <StyledButton onClick={() => goNextPagePending(page)}>Next Pending</StyledButton>
       <ComparisonContainer>
-        <ExperimentContainer>
-          <h2>Pending-Skeleton-Complete</h2>
-          {isPending && <div>Loading dog image...</div>}
-
-          {
-            page % 2 === 0 ? <BoxMatrix
-                times={7}
-                component={<FetchOnRenderBox index={10} timeout={500} />}
-              />
-              : <ImageLoader resource={resource} />
-          }
-
-        </ExperimentContainer>
+        <Receding resource={resource2} />
+        <Pending resource={resource} isPending={isPending} />
       </ComparisonContainer>
     </>
   );
+};
+
+const Pending = ({ isPending, resource }) => {
+  return <ExperimentContainer>
+    <h2>Pending-Skeleton-Complete</h2>
+    {isPending && <div>Loading dog image...</div>}
+    <BoxLoader resource={resource} />
+    <ImageLoader resource={resource} />
+  </ExperimentContainer>;
+};
+
+const Receding = ({ resource }) => {
+  return <ExperimentContainer>
+    <h2>Receding-Skeleton-Complete</h2>
+    <BoxLoader resource={resource} />
+    <ImageLoader resource={resource} />
+  </ExperimentContainer>;
+};
+
+
+const BoxLoader = ({ resource }) => {
+  if (resource) return null;
+  return <BoxMatrix
+    times={7}
+    component={<FetchOnRenderBox index={10} timeout={500} />}
+  />;
 };
 
 const ImageLoader = ({ resource }) => {
